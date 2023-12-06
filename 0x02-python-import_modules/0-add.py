fee881a2545dffe52ad113b7dd3170564fc706f8
Michie98 <mike98ndech@gmail.com>
2023-12-06 09:59:57 -0800

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
-    for nbr in my_list:
-        print("{:d}".format(nbr))
+if __name__ == "__main__":
+    from add_0 import add
+      a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
