b5084e43449c41941ba196c7770a3857b9554728
Michie98 <mike98ndech@gmail.com>
2023-12-08 09:50:05 -0800

    7-update_dictionary.py

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-number = 0
-while number <= 89:
-    if number % 10 == 0:
-        number += 1 + number // 10
-    print("{:02d}".format(number), end='\n' if number == 89 else ", ")
-    number += 1
+
+def print_sorted_dictionary(a_dictionary):
+    if a_dictionary is None:
+        return
+    for k in sorted(a_dictionary.keys()):
+        print("{}: {}".format(k, a_dictionary.get(k)))
